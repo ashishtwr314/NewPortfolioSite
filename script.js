7b52229f60ed3c90af9cf4e22f2108acaa486d5b
Ashish Tiwari <ashish.tiwari@KAYADEV.COM>
2020-10-21 12:23:29 +0530

    Journey section done

--- script.js
+++ script.js
@@ -10,41 +10,41 @@ const journeyTOggle = document.getElementById("journey-toggle");
 const contact = document.getElementById("contact");
 const contactToggle = document.getElementById("contact-toggle");
 
-TweenMax.to(".overlay h1", 3, {
-  opacity: 0,
-  y: -20,
-  ease: Expo.easeInOut,
-});
+// TweenMax.to(".overlay h1", 3, {
+//   opacity: 0,
+//   y: -20,
+//   ease: Expo.easeInOut,
+// });
 
-TweenMax.to(".overlay p", 3, {
-  opacity: 0,
-  y: -60,
-  ease: Expo.easeInOut,
-});
+// TweenMax.to(".overlay p", 3, {
+//   opacity: 0,
+//   y: -60,
+//   ease: Expo.easeInOut,
+// });
 
-TweenMax.to(".overlay", 2, {
-  delay: 1,
-  y: "-100%",
-  ease: Expo.easeInOut,
-});
+// TweenMax.to(".overlay", 2, {
+//   delay: 1,
+//   y: "-100%",
+//   ease: Expo.easeInOut,
+// });
 
-TweenMax.from(".wrapper .left-bg", 2, {
-  delay: 1.6,
-  width: "0%",
-  ease: Expo.easeInOut,
-});
+// TweenMax.from(".wrapper .left-bg", 2, {
+//   delay: 1.6,
+//   width: "0%",
+//   ease: Expo.easeInOut,
+// });
 
-TweenMax.from(".content", 2, {
-  delay: 1.6,
-  x: "100%",
-  ease: Expo.easeInOut,
-});
+// TweenMax.from(".content", 2, {
+//   delay: 1.6,
+//   x: "100%",
+//   ease: Expo.easeInOut,
+// });
 
-TweenMax.from(".nav", 1, {
-  delay: 3,
-  x: "-200px",
-  ease: Expo.easeInOut,
-});
+// TweenMax.from(".nav", 1, {
+//   delay: 3,
+//   x: "-200px",
+//   ease: Expo.easeInOut,
+// });
 
 console.log(document.querySelectorAll(".toggle"));
 for (x = 0; x < document.querySelectorAll(".toggle").length; x++) {
